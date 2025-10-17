d9ba5e5ec74c26bd0d221696a5559a13b51db2f1
rakesh kumar <edgaon82@gmail.com>
2025-10-17 17:10:06 +0530

    i changed in profile screen

--- lib/profile_screen.dart
+++ lib/profile_screen.dart
@@ -2,16 +2,17 @@ import 'package:flutter/material.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({super.key});
-
-
-
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("Profile"),
-      ),
-    );
+      body: Column(children: [
+        Text("data"),
+        Text("data"),
+        Text("data"),
+        Text("data"),
+        Text("data"),
+
+      ],
+      ));
   }
 }
